f1a8690eb5326a8f00f611052a42c0432b48ff6d
elvinEl <yuzbekov79@gmail.com>
2026-02-19 12:48:12 -0500

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Elvın Yuzbayov on 2026-02-19.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
